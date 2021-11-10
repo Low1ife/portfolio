31bdf94d1999f570095c6d679f5efb66869425fb
wooseok <80329046+Low1ife@users.noreply.github.com>
2021-11-10 15:49:21 +0900

    Create base.js

---
+++ assets/js/base.js
@@ -0,0 +1,29 @@
+    // scroll
+    // var s = skrollr.init();
+    // window.addEventListener("scroll", () => {
+    //     let scrollTop = document.documentElement.scrollTop || window.scrollY || window.PageYOffset;
+    //     document.querySelector(".scroll").innerHTML = scrollTop;
+    // });
+
+
+    //가로
+    const contentWrap = $(".contWrap");
+    const contentItem = $(".content-item");
+
+    let scrollTotalWidth = contentWrap.width();
+    $("body").height(scrollTotalWidth);
+        
+    $(window).scroll(function(){
+        let scrollTop = $(window).scrollTop();
+
+        let wWidth = $(window).width();
+        let wHeight = $(window).height();
+
+        let nWidth = scrollTotalWidth - $(window).width();
+        let nHeight = $("body").height() - $(window).height();
+
+        let ratio = nWidth / nHeight;
+        let goLeft = scrollTop * ratio;
+
+        contentWrap.css("left" , -goLeft);
+    });
